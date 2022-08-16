cd9a37287ac63954a91238b9af368ec2aa82d745
suressk <sure_ssk@outlook.com>
2022-08-17 02:28:09 +0800

    chore: ☕️ update `button` component

---
+++ packages/components/button/index.ts
@@ -0,0 +1,8 @@
+import { withInstall } from '@sure-ui/utils'
+import Button from './src/button.vue'
+
+export const SureButton = withInstall(Button)
+
+export default SureButton
+
+export * from './src/button'
--- packages/components/button/src/button.vue
+++ packages/components/button/src/button.vue
@@ -7,12 +7,12 @@ const props = defineProps(buttonProps)
 
 const emit = defineEmits(buttonEmits)
 
-const _ref = ref<HTMLButtonElement>()
-const _type = computed(() => props.type || '')
-
 defineOptions({
   name: 'SureButton'
 })
+
+const _ref = ref<HTMLButtonElement>()
+const _type = computed(() => props.type || '')
 
 defineExpose({
   ref: _ref,
@@ -25,23 +25,18 @@ const handleClick = (evt: MouseEvent) => {
 </script>
 
 <template>
-  <button ref="_ref" @click="handleClick">
+  <button ref="_ref" class="sure-button" @click="handleClick">
     <template v-if="loading">
       <slot v-if="$slots.loading" name="loading" />
-      <sure-icon v-else class="sure-icon sure-icon-loading">
+      <sure-icon v-else loading>
         <component :is="loadingIcon" />
       </sure-icon>
-      <span v-if="loading" class="sure-icon sure-icon-loading" />
     </template>
 
     <sure-icon v-else-if="icon || $slots.icon">
       <component :is="icon" v-if="icon" />
       <slot v-else name="icon" />
     </sure-icon>
-
-    <span>
-      <slot />
-    </span>
 
     <!-- :class="{ [ns.em('text', 'expand')]: shouldAddSpace }" -->
     <span
