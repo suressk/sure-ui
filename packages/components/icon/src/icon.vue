e731236867e59346b9fb456768faa4f575c35fce
suressk <sure_ssk@outlook.com>
2022-08-20 18:29:17 +0800

    chore: ☕️ update `Icon` className

--- packages/components/icon/src/icon.vue
+++ packages/components/icon/src/icon.vue
@@ -2,6 +2,7 @@
 import { computed } from 'vue'
 import type { CSSProperties } from 'vue'
 import { addUnit, isUndefined } from '@sure-ui/utils'
+import { useNamespace } from '@sure-ui/hooks'
 import { iconProps } from './icon'
 
 const props = defineProps(iconProps)
@@ -12,6 +13,8 @@ defineOptions({
   // 继承 attrs（属性）
   inheritAttrs: false
 })
+
+const ns = useNamespace('icon')
 
 const style = computed<CSSProperties>(() => {
   if (!props.size && !props.color) {
@@ -24,11 +27,11 @@ const style = computed<CSSProperties>(() => {
   }
 })
 
-const cls = props.loading ? 'sure-icon sure-icon-loading' : 'sure-icon'
+// const cls = props.loading ? 'sure-icon sure-icon-loading' : 'sure-icon'
 </script>
 
 <template>
-  <i :class="cls" :style="style">
+  <i :class="ns.baseCls()" :style="style" v-bind="$attrs">
     <slot />
   </i>
 </template>
